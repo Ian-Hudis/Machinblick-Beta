e88a1ad5399ada7cb6f0d8c8307036b9b1e3586b
Ian Hudis <52722993+Ian-Hudis@users.noreply.github.com>
2026-01-19 09:32:53 -0600

    Add files via upload

---
+++ Shared/GuiAssist.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Maschinblick.Shared
+{
+    public class GuiAssist
+    {
+
+
+        //! Theme
+            public string selectedTheme = "Normal";// = DataSave.Savedata.Theme;
+            public List<string> themes = new () { "Normal", "Printer Friendly" };
+            public string BarBackground = "#2d2d2d";
+        //!
+
+        //! for menu status
+            public string menustatus = "Hide";
+            public bool _navBarVisible = true;
+
+
+        //get additional css class for nav bar div
+        public string NavBarClass
+        {
+            get
+            {
+                if (_navBarVisible) return string.Empty;//No additional css class for show nav bar
+                return "d-none";//d-none class will hide the div
+            }
+        }
+        //! for menu status
+
+        public System.Action? OnChanged { get; set; }
+
+        //Change state by click on the button
+        public void Toggle()
+        {
+            _navBarVisible = !_navBarVisible;//Change
+
+            if (_navBarVisible)
+            {
+                menustatus = "Hide";
+            }
+            else
+            {
+                menustatus = "Show";
+            }
+
+            OnChanged?.Invoke();//Callback for reload//Callback for reload
+        }
+
+        public bool IsSelectedTheme(string theme)
+        {
+            return theme == selectedTheme;
+        }
+
+        public void ChangingTheme(ChangeEventArgs e)
+        {
+            #pragma warning disable CS8601 // Dereference of a possibly null reference.
+            #pragma warning disable CS8602 // Dereference of a possibly null reference.
+            selectedTheme = e.Value.ToString();
+        }
+        //! Theme
+
+
+    }
+}
---
+++ Shared/TimeFormatting.cs
@@ -0,0 +1,76 @@
+﻿namespace Maschinblick.Shared
+{
+    public class TimeFormatting
+    {
+        // default values for start and end date
+        public string StartDate = DateTime.Today.AddDays(-1).AddHours(5).ToString("yyyy-MM-dd HH:mm:ss"); //= DataSave.Savedata.StartDate;
+        public string EndDate = DateTime.Today.AddHours(5).ToString("yyyy-MM-dd HH:mm:ss"); //= DataSave.Savedata.EndDate;
+
+        public void StartEarlier()  //minus sign
+        {
+            try
+            {
+                DateTime DateStarts = DateTime.Parse(StartDate);
+                DateStarts = DateStarts.AddDays(-1);
+                StartDate = DateStarts.ToString("yyyy-MM-dd HH:mm:ss");
+            }
+            catch
+            {
+                Console.WriteLine("Failed to Parse The Start date (StartEarlier function)");
+            }
+
+        }
+
+        public void StartLater() // plus sign
+        {
+            try
+            {
+                DateTime DateStarts = DateTime.Parse(StartDate);
+                DateStarts = DateStarts.AddDays(1);
+                StartDate = DateStarts.ToString("yyyy-MM-dd HH:mm:ss");
+            }
+            catch
+            {
+                Console.WriteLine("Failed to Parse The Start date (StartLater function)");
+            }
+
+        }
+
+        public void EndEarlier() //minus sign
+        {
+            try
+            {
+                DateTime DateEnds = DateTime.Parse(EndDate);
+                DateEnds = DateEnds.AddDays(-1);
+                EndDate = DateEnds.ToString("yyyy-MM-dd HH:mm:ss");
+            }
+            catch
+            {
+                Console.WriteLine("Failed to Parse The End date (EndEarlier function)");
+            }
+
+        }
+
+        public void EndLater() // plus sign
+        {
+            try
+            {
+                DateTime DateEnds = DateTime.Parse(EndDate);
+                DateEnds = DateEnds.AddDays(1);
+                EndDate = DateEnds.ToString("yyyy-MM-dd HH:mm:ss");
+            }
+            catch
+            {
+                Console.WriteLine("Failed to Parse The End date (EndLater function)");
+            }
+
+        }
+
+        public void SetToNow() // set to now button
+        {
+            EndDate = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+
+    }
+}
